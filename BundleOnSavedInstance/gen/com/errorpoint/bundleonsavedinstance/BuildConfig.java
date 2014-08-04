1ffd6923c69b9c256ce88873cd66a24d2d89fef5
Md. Shahadat Sarker <info@findingsoft.com>
2014-08-04 15:22:18 +0600

    Catch input value when screen rotation / minimizing
    Catch inputed value when screen rotation or minimizing the app

---
+++ BundleOnSavedInstance/gen/com/errorpoint/bundleonsavedinstance/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.errorpoint.bundleonsavedinstance;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
