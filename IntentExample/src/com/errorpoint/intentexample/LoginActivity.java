3f5e0826f553e29204c45cffd2752551f893dd03
Md. Shahadat Sarker <info@findingsoft.com>
2014-10-21 14:42:47 +0600

    Switch one activity to another activity.
    Switch one activity to another activity with Intent.

---
+++ IntentExample/src/com/errorpoint/intentexample/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.errorpoint.intentexample;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		View v = findViewById(R.id.email_sign_in_button);
+		
+		v.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(LoginActivity.this, MainActivity.class));
+				finish();
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
