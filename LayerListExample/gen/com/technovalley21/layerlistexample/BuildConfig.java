216754079030b3461061d64d5ca8ed0b5b77265c
Md. Shahadat Sarker <me.sarker@gmail.com>
2015-05-17 12:50:43 +0600

    Layer-list example in Android
    Layer-list example in Android

---
+++ LayerListExample/gen/com/technovalley21/layerlistexample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.technovalley21.layerlistexample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
