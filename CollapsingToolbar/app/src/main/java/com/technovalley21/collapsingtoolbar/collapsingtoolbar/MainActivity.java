0f137af97a379be76be2ca2ea0a89bb72ebea4a1
Md. Shahadat Sarker <me.sarker@gmail.com>
2016-10-10 13:21:46 +0600

    Collapsing Toolbar Example
    Collapsing Toolbar Layout Example in Android

---
+++ CollapsingToolbar/app/src/main/java/com/technovalley21/collapsingtoolbar/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,26 @@
+package com.technovalley21.collapsingtoolbar.collapsingtoolbar;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.MyToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        collapsingToolbar.setTitle("My Toolbar Tittle");
+
+        collapsingToolbar.setCollapsedTitleTextColor(getResources().getColor(R.color.colorAccent));
+        collapsingToolbar.setExpandedTitleColor(getResources().getColor(R.color.colorPrimaryDark));
+    }
+}
